8c58607e28cf0479b0cc6899fa9f82f878b01db9
585158 <sandesh@terpmail.umd.edu>
2016-10-14 11:24:53 -0400

    First commit

---
+++ src/main/java/helloworld.java
@@ -0,0 +1,20 @@
+/**
+ * Created by sandeshahirtharu on 10/14/16.
+ */
+public class helloworld {
+    public static void main(String args[]){
+        String numStr = printNumber(2);
+        System.out.println(numStr);
+        numStr = printNumber(10);
+        System.out.println(numStr);
+    }
+
+    public static String printNumber(int x){
+        if(x == 1)
+            return "One";
+        else if(x == 2)
+            return "Two";
+        else
+            return "Other";
+    }
+}
