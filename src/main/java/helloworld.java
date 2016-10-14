bcd3b9c42c8083ed9ac8ac16658d0a18a1fb1b69
585158 <sandesh@terpmail.umd.edu>
2016-10-14 15:46:51 -0400

    removing main method

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -2,13 +2,6 @@
  * Created by sandeshahirtharu on 10/14/16.
  */
 public class helloworld {
-    public static void main(String args[]){
-//        String numStr = printNumber(2);
-//        System.out.println(numStr);
-//        numStr = printNumber(10);
-//        System.out.println(numStr);
-    }
-
     public static String printNumber(int x){
         if(x == 1)
             return "One";
