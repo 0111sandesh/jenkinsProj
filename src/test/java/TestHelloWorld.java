141156b6a1564be450ebb3c3ec8687f634212429
585158 <sandesh@terpmail.umd.edu>
2016-10-14 12:16:58 -0400

    add test

---
+++ src/test/java/TestHelloWorld.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class TestHelloWorld {
+    private helloworld hello;
+    @Before
+    public void setUp(){
+        hello = new helloworld();
+    }
+
+    @Test
+    public void test1(){
+        String str1 = hello.printNumber(1);
+        System.out.println(str1);
+        assert(str1.equals("One"));
+    }
+
+    @Test
+    public void test2(){
+        String str2 = hello.printNumber(2);
+        System.out.println(str2);
+        assert(str2.equals("Two"));
+    }
+
+    @Test
+    public void test3(){
+        String str3 = hello.printNumber(3);
+        System.out.println(str3);
+        assert(str3.equals("Random"));
+    }
+
+    @After
+    public void tearDown(){
+        hello = null;
+    }
+}
